5240ef8e65147a33962f4bec7854bb60d3f11336
Anthony Fernandez <anthonyfer0220@gmail.com>
2025-01-27 17:30:42 -0500

    Initial Commit

---
+++ src/main/java/com/github/anthonyfer0220/random_words_generator_api/RandomWordsGeneratorApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.anthonyfer0220.random_words_generator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomWordsGeneratorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomWordsGeneratorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/anthonyfer0220/random_words_generator_api/RandomWordsGeneratorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.anthonyfer0220.random_words_generator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomWordsGeneratorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
