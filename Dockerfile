2df7cc58eb8520c23934c7d4b2673484ab8a54d0
Anthony Fernandez <anthonyfer0220@gmail.com>
2025-01-28 21:19:23 -0500

    Finalize Docker integration

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM eclipse-temurin:21-jdk
+
+RUN mkdir /opt/app
+
+COPY target/random-words-generator-api-0.0.1-SNAPSHOT.jar /opt/app/japp.jar
+
+ENTRYPOINT [ "java", "-jar", "/opt/app/japp.jar" ]
--- src/test/java/com/github/anthonyfer0220/random_words_generator_api/RandomWordsGeneratorApiApplicationTests.java
+++ src/test/java/com/github/anthonyfer0220/random_words_generator_api/RandomWordsGeneratorApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.github.anthonyfer0220.random_words_generator_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class RandomWordsGeneratorApiApplicationTests {
 
 	@Test
